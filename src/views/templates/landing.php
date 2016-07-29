144c4410ba1e811b59b3dc11b201c859316eb03f
iulian_uxb <lulian@uxblondon.com>
2016-07-29 17:04:26 +0100

    Setting up some less predefined mixins

--- src/views/templates/landing.php
+++ src/views/templates/landing.php
@@ -3,14 +3,19 @@
 <article role="main">
     <?= $view('inc.page-header') ?>
 
-    <section class="features">
-        <div class="container">
-        <?= $view('inc.features',[
-            'title' => 'Features',
-            'items' => 6,
-            'class' => 'landing-featured'
-        ]) ?>
-        </div>
+    <section class="features container">
+        <section id="content" class=" main">
+        <?= $chunk('text', 'standfirst') ?>
+        <?= $chunk('text', 'bodycopy') ?>
+        </section>
+          
+          <aside class="sidebar features">
+            <?= $view('inc.features',[
+                'title' => 'Features',
+                'items' => 6,
+                'class' => 'landing-featured'
+            ]) ?>
+        </aside>
     </section>
 </article>
 
