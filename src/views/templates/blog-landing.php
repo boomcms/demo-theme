e7cdf6e0bfe02ef6065f1ed2e98db39b4ebb60e0
iulian_uxb <lulian@uxblondon.com>
2016-08-15 11:59:30 +0100

    Added cta chunk to standard-page.php template

--- src/views/templates/blog-landing.php
+++ src/views/templates/blog-landing.php
@@ -1,9 +1,16 @@
 <?= $view('inc.header') ?>
 
-<article role="main">
-    <?= $view('inc.page-header') ?>
+<article id="main" role="main">
 
-      <section id="content" class=" main">
+        <section class="collection-list">
+        <?= $view('inc.slideshow-list', [
+            'class' => 'page-collection-list',
+            'show_title' => false,
+            'cols' => 3
+        ]) ?>
+    </section>
+
+      <section id="content" class="container">
         <?= $chunk('text', 'standfirst') ?>
         <?= $chunk('text', 'bodycopy') ?>
         </section>
--- src/views/templates/standard-page.php
+++ src/views/templates/standard-page.php
@@ -14,48 +14,10 @@
 
         <h1 id="b-page-title"><?= $page->getTitle() ?></h1>
         <?= $chunk('text', 'standfirst')->setHtml('<h2>{text}</h2>') ?>
-        <?= $chunk('text', 'bodycopy')->setHtml('<main>{text}</main>') ?>
+        <?= $chunk('text', 'bodycopy')->setHtml('<div class="text">{text}</div>') ?>
+        <?= $chunk('asset', 'cta')->template('cta')->setPlaceHolderText('Insert Call to Action') ?>
     </section>
-    <section id="lists">
-    <h2>Remove Me</h2>
-    	<ul class="cols-2"> 
-    		<li></li>
-    		<li></li>
-    		<li></li>
-    		<li></li>
-    	</ul>
-    	<div class="cols-wrapper">
-	    	<ul class="cols-3">
-	    		<li></li>
-	    		<li></li>
-	    		<li></li>
-	    		<li></li>
-	    		<li></li>
-	    		<li></li>
-	    	</ul>
-    	</div>
-    	<ul class="cols-4">
-    		<li></li>
-    		<li></li>
-    		<li></li>
-    		<li></li>
-    	</ul>
-    	<ul class="cols-5">
-    		<li></li>
-    		<li></li>
-    		<li></li>
-    		<li></li>
-    		<li></li>
-    	</ul>
-    	<ul class="cols-6">
-    		<li></li>
-    		<li></li>
-    		<li></li>
-    		<li></li>
-    		<li></li>
-    		<li></li>
-    	</ul>
-    </section>
+
 </article>
 
 <?= $view('inc.footer') ?>
