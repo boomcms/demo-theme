d187b69575b443a1305526287c6b0e8e82ac755f
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-09-28 10:30:04 +0100

    Updated menu to make more dynamic

--- src/views/inc/menu.php
+++ src/views/inc/menu.php
@@ -1,47 +1,29 @@
-<?php 
-  $menu = [
-        '/'  => false,
-        'blog'  => ['parent' => Page::findByPrimaryUri('blog'), 'visibileInNavigation'],
-        'portfolio'  => [],
-        'about'  => [],
-    ];
-?>
+<?php $homepage = $page->isRoot() ? $page : Page::findByPrimaryUri('/') ?>
 
-<?php if (isset($child_items) && ($child_items)) :?>
-    <ul id="topnav" class="main-menu sm sm-simple">
-        <?php foreach ($menu as $section => $criteria) : ?>
-            <?php   
-                $menu_item = Page::findByUri($section); 
-                $menu_sub_items = empty($criteria) ? $getPages(['parent' => $menu_item, 'visibileInNavigation']) : $getPages($criteria)
-            ?>
-                    
-            <?php if ($menu_item && !empty($menu_sub_items)): ?>
-                <?php $has_children = ($criteria !== false) ?>
-       
-                <li <?php if ($menu_item->is($page)): ?> class="active"<?php endif ?>>
-                    <a href="<?= $menu_item->url() ?>">
-                        <?= $menu_item->getTitle() ?>
-                    </a>
+<ul id="topnav" class="main-menu sm sm-simple">
+    <li <?php if ($page->is($homepage)): ?> class="active"<?php endif ?>>
+        <a href="/">Home</a>
+    </li>
 
-                    <?php if ($has_children) : ?>
-                        <ul class="sub-menu">
-                            <?php foreach ($menu_sub_items as $p): ?>
-                                <li id="menu-item-<?= $p->getId() ?>" <?php if ($p->is($page)): ?> class="active"<?php endif ?>>
-                                    <a href="<?= $p->url() ?>" title="<?= $p->getTitle() ?>">
-                                        <?= $p->getTitle() ?>
-                                    </a>
-                              </li>
-                            <?php endforeach ?>
-                        </ul>
-                    <?php endif ?>
-                </li>
+    <?php foreach ($getPages(['parent' => $homepage, 'visibleinnavigation']) as $child): ?>
+        <?php $subpages = $getPages(['parent' => $child, 'visibleinnavigation']) ?>
+
+        <li <?php if ($child->is($page)): ?> class="active"<?php endif ?>>
+            <a href="<?= $child->url() ?>">
+                <?= $child->getTitle() ?>
+            </a>
+
+            <?php if (!empty($subpages)): ?>
+                <ul class="sub-menu">
+                    <?php foreach ($subpages as $p): ?>
+                        <li id="menu-item-<?= $p->getId() ?>" <?php if ($p->is($page)): ?> class="active"<?php endif ?>>
+                            <a href="<?= $p->url() ?>" title="<?= $p->getTitle() ?>">
+                                <?= $p->getTitle() ?>
+                            </a>
+                      </li>
+                    <?php endforeach ?>
+                </ul>
             <?php endif ?>
-       <?php endforeach ?>
-    </ul>
-<?php else : ?>
-    <ul id="topnav">
-        <?php foreach ($getPages(['parent' => Page::findByPrimaryUri('/'), 'visibileInNavigation']) as $p): ?>
-            <li><a href="<?= $p->url() ?>" <?php if ($p->is($page)): ?> class="active"<?php endif ?>><?= $p->getTitle() ?></a></li>
-        <?php endforeach ?>
-    </ul>
-<?php endif ?>
+        </li>
+   <?php endforeach ?>
+</ul>
