5497d3bb92d5cf440d4735c3c8a751b0830df45f
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-09-28 10:47:28 +0100

    Blog page tweaks

--- src/views/templates/blog-page.php
+++ src/views/templates/blog-page.php
@@ -1,7 +1,6 @@
 <?= $view('inc.header') ?>
-<?php $published_date = $page->getLastPublishedTime(); ?>
+
 <article id="main" role="main" >
-     
     <section class="collection-list">
         <?= $view('inc.slideshow-list', [
             'class' => 'page-collection-list',
@@ -16,24 +15,27 @@
         <?= $view('inc.back-to-parent', [
             'back_text' => 'Back to'
         ]) ?>
+
         <h1 id="b-page-title"><?= $page->getTitle() ?></h1>
-        <?php if($published_date) : ?>
-        <h2 class="date"><time datetime="<?= $published_date->format('Y-m-d H:i:s') ?>" pubdate><?= $published_date->format('d M Y') ?></time></h2>
-        <?php endif ?>
+
+        <h2 class="date">
+            <time datetime="<?= $page->getVisibleFrom()->format('Y-m-d H:i:s') ?>" pubdate>
+                <?= $page->getVisibleFrom()->format('d M Y') ?>
+            </time>
+        </h2>
+
         <?= $chunk('text', 'standfirst')->setHtml('<div class="standfirst">{text}</div>') ?>
         <?= $chunk('text', 'bodycopy')->setHtml('<div class="text">{text}</div>') ?>
         <?= $chunk('asset', 'cta')->template('cta')->setPlaceHolderText('Insert Call to Action') ?>
     </section>
-    <?= $view('inc.related-pages',[
+
+    <?= $view('inc.related-pages', [
         'pages' => $getPages([
             'parent' => $page->getParent(),
-            'order' => 'date desc',
             'visibleinnavigation' => true,
-            ]),
-        'limit' => 3
-        ])
-     ?>
-
+            'limit' => 3
+        ]),
+    ]) ?>
 </article>
 
 <?= $view('inc.footer') ?>
