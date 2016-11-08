027614ba8e7f222a0c3e84d5d99617bab3de5dea
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-11-08 13:56:15 +0000

    Removed filename from asset library

--- src/views/chunks/library/album-images.php
+++ src/views/chunks/library/album-images.php
@@ -1,36 +1,31 @@
 <div class="slideshow-list album-images child-cols-3">
-<?php
-$slides = $assets();
-if (count($slides)) : ?>
-<ul>
-<?php foreach ($slides as $slide): ?>
-<?php if ($slide) : ?>
-<?php
-$title = $slide->getTitle();
-$caption = $slide->getDescription();
-$credits = $slide->getCredits();
-// $title = $caption = '';
-// dd(get_class_methods($slide));
-?>
-<li>
-   <a href="<?= $assetURL(['asset' => $slide->getId(), 'width' => 1600]) ?>" class="album-item" <?php if($title || $caption) : ?>data-sub-html=".description"<?php endif ?>><img src="<?= $assetURL(['asset' => $slide->getId(), 'width' => 600]) ?>" alt="<?= $title ?>" />
+    <?php
+        $assets = $assets();
+        if (count($assets)):
+    ?>
+        <ul>
+            <?php foreach ($assets as $asset): ?>
+                <?php
+                    $caption = $asset->getDescription();
+                    $credits = $asset->getCredits();
+                ?>
 
-    <?php if($credits) : ?>
-       <span class="credits"><?= $credits ?></span>
-       <?php endif; ?> 
-    <?php if($title || $caption) : ?><div class="description">
-    <?php if($title) : ?>
-         <h3 class="slide-title"><?= $title ?></h3>
+                <li>
+                    <a href="<?= $assetURL(['asset' => $asset, 'width' => 1600]) ?>" class="album-item" <?php if ($caption): ?>data-sub-html=".description"<?php endif ?>>
+                        <img src="<?= $assetURL(['asset' => $asset, 'width' => 600]) ?>" alt="<?= $asset->getTitle() ?>" />
+
+                        <?php if($credits) : ?>
+                            <span class="credits"><?= $credits ?></span>
+                        <?php endif ?> 
+
+                        <?php if($caption): ?>
+                            <div class="description">
+                                <p class="slide-caption"><?= $caption ?></p>
+                            </div>
+                        <?php endif ?>
+                    </a> 
+                </li>   
+            <?php endforeach ?>
+        </ul>
     <?php endif ?>
-    <?php if($caption) : ?>
-         <p class="slide-caption"><?= $caption ?></p>
-         
-    <?php endif?>    
-       </div>
-    <?php endif ?>
-    </a> 
-</li><?php endif ?>    
-<?php endforeach ?>
-</ul>
-<?php  endif ?>
 </div>
--- src/views/templates/album.php
+++ src/views/templates/album.php
@@ -1,32 +1,35 @@
 <?= $view('inc.header', ['album' => true]) ?>
+
 <article id="main" role="main">
-	<section id="content" class="container">
-	<?= $view('inc.back-to-parent', [
+    <section id="content" class="container">
+
+    <?= $view('inc.back-to-parent', [
             'back_text' => 'Back to'
         ]) ?>
-		<h1 id="b-page-title"><?= $page->getTitle() ?></h1>
-		<?= $chunk('text', 'standfirst') ?>
+        <h1 id="b-page-title"><?= $page->getTitle() ?></h1>
+        <?= $chunk('text', 'standfirst') ?>
         <?= $chunk('text', 'bodycopy')->setHtml('<div class="text">{text}</div>') ?>
-	</section>
+    </section>
 
-	<section class="album-list">
-	    <?= $view('inc.slideshow-list', [
-	        'class' => 'album-images',
-	        'show_title' => false,
-	        'cols' => 3,
-	        'item_template' => 'list-image',
-	        'placeholder_text' => 'Insert album assets'
-	    ]) ?>
-	</section>
-	<section class="album-list">
-		<?= $view('inc.library-list', [
-	        'class' => 'album-images',
-	        'show_title' => false,
-	        'cols' => 3,
-	        'item_template' => 'album-images',
-	        'placeholder_text' => 'Insert album asset library'
-	    ]) ?>
-	</section>
+    <section class="album-list">
+        <?= $view('inc.slideshow-list', [
+            'class' => 'album-images',
+            'show_title' => false,
+            'cols' => 3,
+            'item_template' => 'list-image',
+            'placeholder_text' => 'Insert album assets'
+        ]) ?>
+    </section>
 
+    <section class="album-list">
+        <?= $view('inc.library-list', [
+            'class' => 'album-images',
+            'show_title' => false,
+            'cols' => 3,
+            'item_template' => 'album-images',
+            'placeholder_text' => 'Insert album asset library'
+        ]) ?>
+    </section>
 </article>
+
 <?= $view('inc.footer', ['album' => true]) ?>
