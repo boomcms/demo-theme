4135754f1a128840ae1b8fccfaf446c76f85feb1
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-11-08 13:11:38 +0000

    Removed dates from collection template

--- src/views/templates/collection.php
+++ src/views/templates/collection.php
@@ -1,33 +1,26 @@
 <?= $view('inc.header') ?>
 
 <article id="main" role="main">
-        <section id="content" class="container">
+    <section id="content" class="container">
         <?= $view('inc.siblings-nav',[
-        	'back_text' => 'More from'
+            'back_text' => 'More from'
         ]) ?>
+    
         <h1 id="b-page-title"><?= $page->getTitle() ?></h1>
         <?= $chunk('text', 'standfirst') ?>
-       
         <?= $chunk('text', 'bodycopy') ?>
-        </section>
+    </section>
 
-       <?php 
-       /** 
-       *    Use dfferent views if different structure. Only layout features variables should be used like 'show_title', 'cols', etc
-       */ 
-      ?>
-
-            <?= $view('inc.child-pages', [
-                'pages' => $getPages([
-                    'parent' => $page,
-                    'visibleinnavigation' => true,
-                    ]),
-                'class' => 'child-album-pages',
-                'show_title' => false,
-                'cols' => 3
-            ]) ?>
+    <?= $view('inc.child-pages', [
+        'pages' => $getPages([
+            'parent' => $page,
+            'visibleinnavigation' => true,
+        ]),
+        'class'      => 'child-album-pages',
+        'show_title' => false,
+        'cols'       => 3,
+        'dates'      => false,
+    ]) ?>
 </article>
 
 <?= $view('inc.footer') ?>
-
-
