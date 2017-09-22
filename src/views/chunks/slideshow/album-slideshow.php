6183d5e3a41d4ef83fdcbbccf9dcd91fae8e0781
Rob Taylor <rob@uxblondon.com>
2017-09-22 10:40:00 +0100

    Fixed layout issue in album slideshow

--- src/views/chunks/slideshow/album-slideshow.php
+++ src/views/chunks/slideshow/album-slideshow.php
@@ -5,9 +5,11 @@
                 <a href="<?= $slide->getLink() ?>" class="slide-link card">
                     <span class="bgimage" data-asset="<?= $slide->getAssetId() ?>"></span>
 
-                    <div class="description">
-                        <h3 class="slide-title"><?= $slide->getTitle() ?></h3>
-                        <p class="slide-caption"><?= $slide->getCaption() ?></p>
+                    <div class="container">
+                        <div class="description">
+                            <h3 class="slide-title"><?= $slide->getTitle() ?></h3>
+                            <p class="slide-caption"><?= $slide->getCaption() ?></p>
+                        </div>
                     </div>
                 </a>
             </li> 
