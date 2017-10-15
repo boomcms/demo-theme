52f0f40a6762de2617ed55aab1d0a54a757b3083
Rob Taylor <rob@robbytaylor.co.uk>
2017-10-15 18:30:19 +0100

    Removed credits from album-images

--- src/views/chunks/library/album-images.php
+++ src/views/chunks/library/album-images.php
@@ -4,10 +4,6 @@
             <li>
                 <a href="<?= $assetURL(['asset' => $asset]) ?>" data-sub-html="<h3><?= $asset->getDescription() ?>">
                     <span class="bgimage" data-asset="<?= $asset->getId() ?>"></span>
-
-                    <?php if (!empty($asset->getCredits())): ?>
-                        <span class="credits"><?= $asset->getCredits() ?></span>
-                    <?php endif ?>
                 </a> 
             </li>   
         <?php endforeach ?>
