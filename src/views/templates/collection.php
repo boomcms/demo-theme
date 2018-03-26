3ac50ef49c2b09032aab7becdbd219cb9ec9c14b
Hasan Tareque <hasan@uxblondon.com>
2018-03-26 16:55:03 +0100

    duplicate nav bug fixed

--- src/views/templates/collection.php
+++ src/views/templates/collection.php
@@ -1,4 +1,4 @@
-<?= $view('inc.header') ?><?= $view('inc.header') ?>
+<?= $view('inc.header') ?>
 <?= $chunk('slideshow', 'collectionlist')->template('album-slideshow')->setPlaceHolderText('Insert album assets') ?>
 <?= $view('inc.page-content') ?>
 
